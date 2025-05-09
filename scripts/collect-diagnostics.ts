e178404c867f3967e3f29981c20492de0c9d3b48
Mykhailo Aliinyk <m.aliynik@gmail.com>
2025-05-09 21:44:17 +0100

    6.0.0 - Add new scripts and dependencies for diagnostics and setup
    Introduced a `prepare` script for `husky` and added the `diagnostics` script. Added new dependencies (`husky`, `tsx`, `@types/node`) and upgraded various existing dependencies for improved functionality and maintenance.

---
+++ scripts/collect-diagnostics.ts
@@ -0,0 +1,51 @@
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+
+const toCamelCase = (str: string): string =>
+  str
+    .replace(/[\/]/g, '') // remove slashes
+    .split(/[\s_]+/)
+    .map((word, index) =>
+      index === 0
+        ? word.charAt(0).toLowerCase() + word.slice(1)
+        : word.charAt(0).toUpperCase() + word.slice(1),
+    )
+    .join('');
+
+const diagnosticsDir = path.join(__dirname, '..', 'diagnostics');
+fs.mkdirSync(diagnosticsDir, { recursive: true });
+
+const raw = execSync(
+  'node --max-old-space-size=4096 ./node_modules/typescript/bin/tsc --project ./tsconfig.build.json --extendedDiagnostics',
+  { encoding: 'utf8' },
+);
+
+const metrics: Record<string, number | string> = {};
+
+for (const line of raw.split('\n')) {
+  const match = line.match(/^(.+?):\s+([\d.]+)([A-Za-z]*)$/);
+  if (!match) {
+    continue;
+  }
+
+  let [, key, value, unit] = match;
+
+  key = toCamelCase(key.trim());
+
+  let num = parseFloat(value);
+  if (unit === 'K') {
+    // convert kilobytes to bytes
+    num *= 1024;
+  }
+
+  if (unit === 'M') {
+    num *= 1024 * 1024;
+  }
+
+  metrics[key] = Math.round(num * 1000) / 1000;
+}
+
+const output = JSON.stringify(metrics, null, 2);
+
+fs.writeFileSync(path.join(diagnosticsDir, `latest.json`), output);
