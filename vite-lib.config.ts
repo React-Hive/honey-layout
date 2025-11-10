9d9db1f3fe2333b6a775d472e25c26eed367ce83
Mykhailo Aliinyk <m.aliynik@gmail.com>
2025-11-10 22:29:52 +0000

    9.4.1 - Updated dependencies and removed `@react-hive/honey-style` from external rollup config

--- vite-lib.config.ts
+++ vite-lib.config.ts
@@ -24,7 +24,7 @@ export default {
       fileName: 'index',
     },
     rollupOptions: {
-      external: ['react', '@react-hive/honey-style'],
+      external: ['react'],
     },
   },
 } satisfies UserConfig;
