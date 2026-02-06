014eede4b8251061ce3f8d376b06d53f27665dfc
Mykhailo Aliinyk <m.aliynik@gmail.com>
2026-02-06 21:09:19 +0000

    13.0.0 - Removed `flattenNestedList`, `searchFlattenedItems`, and related types from `@react-hive/honey-layout` as they are no longer maintained. Updated `@react-hive/honey-utils` to v3.22.0, cleaned up deprecated types and tests, upgraded `pnpm-lock.yaml`, and incremented package version for breaking change.

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -211,7 +211,10 @@ export const bpMedia = (
 ) => {
   const resolveBpValue = (theme: HoneyTheme) => {
     const value = theme.breakpoints[breakpoint];
-    assert(value, `[honey-layout]: Setup for breakpoint "${breakpoint}" was not found.`);
+    assert(
+      value,
+      `[@react-hive/honey-layout]: Setup for breakpoint "${breakpoint}" was not found.`,
+    );
 
     return value;
   };
--- src/types/data.types.ts
+++ src/types/data.types.ts
@@ -4,7 +4,7 @@
 import type { RefObject } from 'react';
 import type { HoneyStyledFunction } from '@react-hive/honey-style';
 
-import type { KeysWithNonArrayValues, Nullable } from './utility.types';
+import type { Nullable } from './utility.types';
 import type { HoneyKeyboardEventCode } from './dom.types';
 
 export type HoneyEffectResultFn<Props extends object> = HoneyStyledFunction<Props>;
@@ -105,40 +105,3 @@ export interface HoneyActiveOverlay {
     e: KeyboardEvent,
   ) => void;
 }
-
-/**
- * Represents an item that has been flattened from a hierarchical data structure, with additional
- * properties to support tracking its position and relationships within the hierarchy.
- *
- * This type is particularly useful for scenarios where nested data structures, such as trees or
- * lists with sub-items, need to be transformed into a flat structure while preserving the depth
- * and parent-child relationships.
- *
- * @template OriginItem - The type of the original item from the hierarchical structure.
- * @template NestedListKey - The key within `OriginItem` that contains nested items or lists.
- */
-export type HoneyFlattenedItem<OriginItem extends object, NestedListKey extends string> = Omit<
-  OriginItem,
-  // Remove `NestedListKey` from the keys of `Item`
-  NestedListKey
-> & {
-  /**
-   * The optional id of the parent item in the flattened structure. This establishes the parent-child
-   * relationship and allows the reconstruction of the original hierarchy if needed.
-   */
-  parentId: OriginItem[KeysWithNonArrayValues<OriginItem>] | undefined;
-  /**
-   * The depth level of the item in the flattened structure. This indicates how deep the item is nested
-   * within the hierarchy, starting from 0 for top-level items.
-   *
-   * @default 0
-   */
-  depthLevel: number;
-  /**
-   * The total number of nested items that are contained within the current item. This helps to keep
-   * track of the overall size of the nested structure for each item.
-   *
-   * @default 0
-   */
-  totalNestedItems: number;
-};
